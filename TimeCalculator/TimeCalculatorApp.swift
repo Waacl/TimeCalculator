cc22b1ade798dffc2f26c0f4db0994323dab2b09
Alexander Waclawczyk <github@waclawczyk.net>
2023-10-01 19:27:21 +0200

    Initial Commit

---
+++ TimeCalculator/TimeCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeCalculatorApp.swift
+//  TimeCalculator
+//
+//  Created by Alex on 01.10.23.
+//
+
+import SwiftUI
+
+@main
+struct TimeCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
